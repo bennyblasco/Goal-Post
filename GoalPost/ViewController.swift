4bd0b151c909f850612bdd534d2b1c54a343b2ef
Benjamin Inemugha <bennyblasco@gmail.com>
2020-06-28 21:21:32 +0100

    Initial Commit

---
+++ GoalPost/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoalPost
+//
+//  Created by Benjamin Inemugha on 28/06/2020.
+//  Copyright © 2020 Techelopers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
