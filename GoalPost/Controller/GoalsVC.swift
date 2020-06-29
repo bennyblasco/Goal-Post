3563d1c226454a990d314d4d46f6ef39a0a79763
Benjamin Inemugha <bennyblasco@gmail.com>
2020-06-29 22:48:17 +0100

    Storyboard formatting

--- GoalPost/Controller/GoalsVC.swift
+++ GoalPost/Controller/GoalsVC.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class GoalsVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func addGoalBtnWasPressed(_ sender: Any) {
+        print ("Button was pressed")
+    }
+    
 }
 
