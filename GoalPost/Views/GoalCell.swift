8e787323881a1d9b56cd1ef723ce522199d5b683
Benjamin Inemugha <bennyblasco@gmail.com>
2020-06-30 06:09:11 +0100

    Goal Cell UI setup

---
+++ GoalPost/Views/GoalCell.swift
@@ -0,0 +1,24 @@
+//
+//  GoalCell.swift
+//  GoalPost
+//
+//  Created by Benjamin Inemugha on 30/06/2020.
+//  Copyright © 2020 Techelopers. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
