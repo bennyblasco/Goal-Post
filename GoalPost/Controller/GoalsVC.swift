060dda442b1504df66ac753feac38d01eb3edea9
Benjamin Inemugha <bennyblasco@gmail.com>
2020-07-01 09:52:30 +0100

    Creating Goal UI View

--- GoalPost/Controller/GoalsVC.swift
+++ GoalPost/Controller/GoalsVC.swift
@@ -7,13 +7,17 @@
 //
 
 import UIKit
+import CoreData
 
 class GoalsVC: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.isHidden = false
+        
     }
 
     @IBAction func addGoalBtnWasPressed(_ sender: Any) {
@@ -22,3 +26,17 @@ class GoalsVC: UIViewController {
     
 }
 
+
+extension GoalsVC: UITableViewDelegate, UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "goalCell") as? GoalCell else{return UITableViewCell()}
+        cell.configureCell(description: "Eat Salad twice a week", type: .longTerm, goalProgressAmount: 4)
+        return cell
+    }
+}
